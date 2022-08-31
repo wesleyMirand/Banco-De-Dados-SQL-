e8f380a9859effa5c020b3ae50437882afc6bd0c
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2022-08-31 16:21:07 -0300

    Update BancodeDadosPart2.sql

--- BancodeDadosPart2.sql
+++ BancodeDadosPart2.sql
@@ -1,1 +1,46 @@
-
+                                                   Banco De Dados
+                                                   
+                              Após a evolução extrema, do papel para o computador e a redução
+                              do tamanho desses computadores. Para continuar o progresso foi 
+                              criado: Disquetes, Hd.s, Primeiro "Winchester"(Na epoca era esse nome).
+                              E Esses tipo de mecanismo começou a analisar dados, de forma direta,
+                              deixando pra trás o sequencial, pra achar um arquivo ou dados você
+                              poderia ir direto nele.
+                              
+                              Esses "Mecanismo de Armazenamento", eram possivel alocar números, registros
+                              e manter numa tabela que poderia possuir, indices, chaves identificadora
+                              dos registro. Aparti dai ganha nome de Indice, quando o arquivo é anexado 
+                              direto e indexado, a forma de encontrar dados se tornou rápida. Ganhando 
+                              nome de "Arquivo de Modo Direto".
+                              
+                              Arquivo de acesso sequencial: São onde os registro é acessado sequencialmente
+                              
+                              Arquvio de acesso direto: São arquivos que podem ser acessado de maneira direta
+                              
+                              Mesmo com arquivos direto, sendo melhor que o sequencial. Ainda tinha o problema do
+                              indice ser simplis demais. Até que na epoca de 60, o Departamento do Estados Unidos 
+                              começou a trabalhar para melhor o sistema, para armazenar dados de maneira segura
+                              e inteligente.
+                              
+                              Até que então eles criaram um documento chamado "CODASYL",foi um encontro de militares,
+                              empressas e universitarios. Para discutir melhorias da Tecnologia, que poderiam ser criadas.
+                              Até então surgiu a linguagem "COBOL", que se preocupava com a lógica e a estruturas de dados,
+                              e assim foi criado o "Banco de Dados". Assim foi composto de "quatro" dados que são:
+                              
+                              Base de Dados: Dados dito, estrutura de banco de dados
+                              Sistema Gerenciador: Famoso pela Sigla "SGBD" sistema de gerenciador de Banco de Dados,em inglês é
+                              "DMS". Dentro de todo ecossistema do computador, tenquer ter um sistema que gerencie todos os dados.
+                              
+                              Linguagem de Exploração: Linguagem de acesso ao dado, exploração.
+                              
+                              Programas Adicionais: Gerencia de Usúario,utilizador de dados.
+                              
+                              Além disso a IBM, foi fundamental pro Banco de Dados, ela propois "Dados Hierarquios", os dados iam
+                              ser armazenados e ia ter uma hierarquia, teria dados interligados de uma maneiroa Simplistica. e fico 
+                              conhecido como modelo "Hierarquico". 
+                              
+                              Além dele foi proposto outro modelo, o modelo em rede, que não existia superior e inferior eles eram
+                              ligados numa forma de Rede Inteligente
+                             
+                              
+                              
