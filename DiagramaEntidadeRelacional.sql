d3e0fd9fb95d7cc89952eef05192695f08b00d7d
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2022-08-31 17:41:07 -0300

    Create DiagramaEntidadeRelacional.sql

---
+++ DiagramaEntidadeRelacional.sql
@@ -0,0 +1,15 @@
+                                              Diagrama Entidade Relacional
+                                              
+                                              
+                            Um grande exemplo que temos, é quando recebe mensagem de erro ao tentar
+                            usar, um nome de usúario. Isso ocorre porque, o nome escolhido ja está
+                            em uso. E os comercio que controlam umonte de pedidos, clientes, cartão.
+                            As resposta estão no banco de dados, coleção de informações organizadas,
+                            que o dados possa ser gerenciado usado, atualizado e recuperado. E para 
+                            entender como esses dados relacionam entre si, os engenheiros de dados
+                            precisam, de um auxilio visual para entender, como cada um desses dados
+                            estão relacionado e entender como eles funcionam junto e para mostrar
+                            isso. Foi criado o "DIAGRAMA ENTIDADE-RELACIONAL".
+                            
+                            
+                            
