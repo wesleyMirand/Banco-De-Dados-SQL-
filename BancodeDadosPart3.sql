d1f1fedc25e9ac6d01ce5f8a6df69e63be74b26c
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2022-08-31 16:44:57 -0300

    Create BancodeDadosPart3.sql

---
+++ BancodeDadosPart3.sql
@@ -0,0 +1,36 @@
+                                                    A IBM no banco de Dados foi super fundamental. 
+                             
+                              
+                              Além disso a IBM, foi fundamental pro Banco de Dados, ela propois "Dados Hierarquios", os dados iam
+                              ser armazenados e ia ter uma hierarquia, teria dados interligados de uma maneiroa Simplistica. e fico 
+                              conhecido como modelo "Hierarquico". 
+                              
+                              Além dele foi proposto outro modelo, o modelo em rede, que não existia superior e inferior eles eram
+                              ligados numa forma de Rede Inteligente, Trazendo o modelo em rede.
+                              
+                              Os dois modelos permitiam que guardasse dados do cliente, serviço, funcionário, empresas e fornecadores.
+                              O problema que esse modelo, não ajudava no relacionamento, dados entre conjunto de registro a outro registro
+                              que está em outra tabela. E pensando nisso os pesquisadores da IBM, propôs o novo modelo, uma nova paradigma.
+                              
+                              Ao invés de Hierarquia ou Ligações de redes, eles teriam relação. E assim surgiu o modelo Relacional, modelo
+                              usado pra criar banco de dados em "MySql".
+                              
+                              Temos modelos mais recentes que são baseados em Documento e Orientado a objetos, porém na faculdade começa pelo
+                              relacional. Esse modelo relacional é importante e permite coisa do tipo:
+                              
+                              eu tenho: Banco de Dados e tenho Cadastro de algum Cliente, vou pegar essa ficha e guardar no meu Banco De Dados.
+                              dai eu posso ter acesso a todos dados do meu cliente.
+                              
+                              Porém com qualquer modelo, eu posso fazer isso. Mais o "MODELO RELACIONAL" da um passo a frente. Eu poderia 
+                              identificar onde o "cliente" mora e todas "compra" que ele fez. E aparti das compra feita posso ter "acesso" da 
+                              data que foi feita e qual "influencia" ela teve no meu "estoque". E se o estoque ficar baixo, eu posso ter "acesso"
+                              direto ao meu "fornecedor". Assim se torna um "paradigma valioso", aparti de um dado eu posso caminhar por eles,
+                              contanto que eles tenham relação entre si. E eu como construtor de Banco de Dados especifique, essa relação. Até 
+                              então vem a "Linguagem de Exploração" do banco de dados. Essa "Linguagem Especifica" ja teve varios nomes, o primeiro
+                              foi "Structured English Query Language", que ficou "SEQUEL", burca bem. Até mudar para "Structured Query Language", 
+                              famosa "SQL". Ela é uma linguagem de consulta, que faz comando, da instrução do ambiente de banco de dados, que
+                              solicita até ter uma respota da solicitação que é uma "QUERY" um resposta.
+                              
+                              
+                              
+                             
