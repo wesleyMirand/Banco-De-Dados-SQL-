ec4a81634409e72c621396c89190bede258b2152
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2022-08-31 17:03:09 -0300

    Create BancodeDadosPart4.sql

---
+++ BancodeDadosPart4.sql
@@ -0,0 +1,24 @@
+                                           O inicio do SQL
+                                           
+                    A ideia principal, era quem a "SQL" fosse uma linguagem universal,uma unica linguagem sql,
+                    e todos os banco de dados a suportaria com comandos.
+                    
+                    Até cada fabricante criar sua própria "SQL", deixando o mercado confuso. Cada banco de dados
+                    tinha sua "SQL", mais dava uma apimentada nos comando, que gerou um uma depadronização.
+                    E para isso ter ter solução criaram o "Orgão de Padronização". Os orgão americano "ANSI" e "ISO"
+                    entram na briga pra padronizar. Assim se no mercado se deparar com algum dos dois, é porque
+                    eles sao compativel com o "SQL PADRONIZADO Universalmente".
+                    
+                    Até entao surgiu varios banco de dados, por exemplo o "ORACLE", um dos maiores no mercado. Além 
+                    deles a "Db2", que criou o banco de dados, tem o "dBase" que tem nas faculdades e por ultimo o da 
+                    microsoft "SQL SERVER". As quatro soluções são pagas e empresariais.
+                    
+                    Um dos banco de dados gratuito e que pertenca a "ORACLE" mais é gratuito é o: 
+                    "MYSQL" uma das soluções mais populares, e quem varias ferramentas.
+                    
+                    E tem mais curiosidades, o grupo que crio a "MySql" pra "Oracle", esta criando um "FORK", versão
+                    mais atualizada separada do "SQL" é a "MARIADB" que foi criada no final de 2015 pra 2016. Além deles
+                    vem o "FireBird" e o "PostgreSql". Todas essas soluções, são de banco de dados e  quatro são pagas, quatro gratuita.
+                    
+                                           
+                          
