4dfee58933bcf9e206a07c224f1831dbb1c2fd87
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2022-08-31 15:27:24 -0300

    Create OqueÉBancoDeDados.sql

---
+++ OqueÉBancoDeDados.sql
@@ -0,0 +1,34 @@
+
+                                                              Banco De Dados
+                                 
+                                 
+                           A origem do "Banco De Dados", foi na epoca de "50", com os computadores pra uso militares e academico.
+                           Antes do computador os dados eram prenchidos em papel, depois armazenados em pasta normal e guardado em
+                           um arquivo metálico(armario de gavetas).          
+                           
+                           A "ficha, Pasta e Arquivo", tem nomes especificos na area de Tecnologia.
+                           
+                           Ficha = São tratas como Registro
+                           
+                           Pasta = Tabelas
+                           
+                           Armarios com gavetas = Arquivos ( Os armários grande guardam pastas, essas pastas guardam as ficha.
+                           na aréa de TI:
+                           
+                           Arquivos: Tabela
+                           
+                           Tabelas: Armazenam Registro
+                           
+                           Devido o aucumulo de papel, o grande desafio da epoca de 50, 60 era digitalizar todos esses dados
+                           que estavam no papel. Isso levou em conta, a expanção da computação nas empressas. Os computadores
+                           universitários e militares eram gigante até começarem a reduzir a ponto de ganhar espaço nas empressas.
+                           Até então virou necessário, guarda todos dados, no espaço digital.Logo no inicio começou a dar problemas,
+                           porque tentavam colocar todas fichas juntas, porque os arquivos passado eram guardado com perfuração e 
+                           esses meio de armazenamento eram sequenciais para voce ler um cartão perfurado tinha que ler o primeiro até
+                           chegar no desejavel e a mesma coisa era com as fita maginetica. E então os arquivos armazenado no computador
+                           eram a mesma coisa, pra achar um, tinha que começa no começo até o encontralo. Até então ganhou nome de
+                           "Arquivos Sequenciais", porque os dados eram guardados em uma sequência. E trazia uma lentidão porém era melhor
+                           que no papel.
+                           
+                         
+                           
