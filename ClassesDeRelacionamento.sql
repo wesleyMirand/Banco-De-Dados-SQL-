724d0b5dd88957db048f9a6b454a8383461d7bac
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2022-09-01 11:28:56 -0300

    Create ClassesDeRelacionamento.sql

---
+++ ClassesDeRelacionamento.sql
@@ -0,0 +1,17 @@
+ |motorista      |   |Veiculo    |                   |Agenda       |         |Multa        |
+ |  nome         |   |TipoVeiculo|                   |NumCarga     |         |IdMulta      |
+ |  cnh          |   |placa      |                   |Placa_veiculo|         |Placa_veiculo|
+ |tipoHabilitacao|   |capacidade |                   |cnh_Motorista|         |TipoMulta    |
+ |DataContratação|        |                          |Peso         |         |horario      |
+         ^                |------------------------->|saida        |         |gravidade    |
+         |                                           |retorno      |         |Pontuação    |
+         |                                              ^       ^            |custo        |
+         |----------------------------------------------|       |                  |
+                                                                -------------------|
+                                                                
+                                                                
+                                                                
+                                                                
+                                                                Acima se encontra,um problema resolvido
+                                                                para achar quem levou multa numa empresa
+                                                                de transporte.
